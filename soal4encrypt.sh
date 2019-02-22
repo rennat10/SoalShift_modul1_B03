f806f34ed36fa42a7e9e3f40f544bc6334a46d95
Zidan Lutfi <36794342+lutzid@users.noreply.github.com>
2019-02-22 17:59:54 +0700

    Update soal4encrypt.sh

--- soal4encrypt.sh
+++ soal4encrypt.sh
@@ -3,16 +3,16 @@
 hKecil=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 hBesar=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 
+#mendapatkan menit, jam, tanggal, bulan, tahun
 menit=`date "+%X" | awk -F: '{print $2}'`
 jam=`date "+%X" | awk -F: '{print $1}'`
 tanggal=`date | awk '{print $3}'`
 bulan=`date | awk '{print $2}'`
 tahun=`date | awk '{print $6}'`
 
+#menggeser indeks untuk mendapatkan kombinasi enkripsi
 kecila1=${hKecil[$jam]}
 kecilb1=${hKecil[$((jam-1))]}
-echo "$kecila"
-echo "$kecilb"
 besara1=${hBesar[$jam]}
 besarb1=${hBesar[$((jam-1))]}
 
